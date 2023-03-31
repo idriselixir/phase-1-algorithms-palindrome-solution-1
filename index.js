bdf25fcdedfb2d17f8866e1fdad63e29ade2e343
idris <idreesnoor150@gmail.com>
2023-03-31 12:15:55 +0300

    done

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const length = word.length;
+  for (let i = 0; i < length / 2; i++) {
+    if (word[i] !== word[length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+
 }
+
 
 /* 
   Add your pseudocode here
